3304b9b5751a4777ab3cf823d16986da10bd1c9b
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-06-27 20:59:47 +0200

    Finished management pages styling

--- src/main/java/edu/umcs/bookystore/controllers/LibraryController.java
+++ src/main/java/edu/umcs/bookystore/controllers/LibraryController.java
@@ -7,13 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import edu.umcs.bookystore.dtos.BookDto;
 import edu.umcs.bookystore.entities.AuthorEntity;
 import edu.umcs.bookystore.entities.BookEntity;
 import edu.umcs.bookystore.entities.CategoryEntity;
